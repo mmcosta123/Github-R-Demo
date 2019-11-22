4978c4bc3217c61c913b9b62c9e95b976ef5ea50
mmcosta123 <mendescosta@icloud.com>
2019-11-22 12:14:16 +0000

    Add sdfsf

---
+++ README.md.R
@@ -0,0 +1,12 @@
+# Github-R-Demo
+
+## Chick Weight Data
+
+The 'ChickWeight' are analysed as follows:
+  
+  + Summary statistics for weight by Diet and time
++ Box whisker plots by Diet over Time for weight
+
+## Further information
+
+Please see https://islustat.com/shared/Getting-Started-in-R.pdf
